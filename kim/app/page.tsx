7db9c84ab24b34c8c72747c505f79e1f085603bb
kimwoojin123 <kimwoojin1903@gmail.com>
2023-12-19 18:10:18 +0900

    팀원소개 페이지 간단히 작성

--- kim/app/page.tsx
+++ kim/app/page.tsx
@@ -1,19 +1,31 @@
+'use client';
 import styles from './page.module.css'
-import Link from 'next/link';
+import { useState} from 'react';
 
 
 export default function Home() {
+  const [section, setSection] = useState('');
+
+  const nameClick = (targetSection) => {
+    setSection(targetSection);
+    window.location.hash = `#${targetSection}`;
+  };
+
   return (
     <main className={styles.main}>
       <div className={styles.head}>
         <ul>
-          <Link href="#kim"><li>김우진</li></Link>
-          <Link href="#bang"><li>방승희</li></Link>
-          <Link href="#jung"><li>정영식</li></Link>
-          <Link href="#choi"><li>최성민</li></Link>
+        <li onClick={() => nameClick('kim')}>김우진</li>
+          <li onClick={() => nameClick('bang')}>방승희</li>
+          <li onClick={() => nameClick('jung')}>정영식</li>
+          <li onClick={() => nameClick('choi')}>최성민</li>
         </ul>
       </div>
       <div className={styles.board}>
+        <div id="kim" style={{ display: section === 'kim' ? 'block' : 'none' }}>김우진 섹션</div>
+        <div id="bang" style={{ display: section === 'bang' ? 'block' : 'none' }}>방승희 섹션</div>
+        <div id="jung" style={{ display: section === 'jung' ? 'block' : 'none' }}>정영식 섹션</div>
+        <div id="choi" style={{ display: section === 'choi' ? 'block' : 'none' }}>최성민 섹션</div>
       </div>
     </main>
   )
